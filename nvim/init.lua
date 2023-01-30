b30e64a032881a185e090c4f3f9a8af928b3c8d0
Dan Strokirk <dan.strokirk@gmail.com>
2023-01-30 17:47:38 +0100

    nvim: Add tmp mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,15 @@ require("lazy").setup({ { import = "plugins" } }, { change_detection = { notify 
 -- Colorscheme
 vim.cmd("silent! colorscheme molokai")
 
+-- Temporary mappings during experimentation with these plugins
+vim.keymap.set("n", "<leader>s", ":Telescope grep_string<cr>")
+vim.keymap.set("n", "<leader>t", ":Telescope<cr>")
+vim.keymap.set("n", "<leader>kp", ":CopyFilename<cr>")
+vim.keymap.set("n", "<leader>xx", ":TroubleToggle<cr>")
+vim.keymap.set("n", "<leader>xd", ":TroubleToggle document_diagnostics<cr>")
+vim.keymap.set("n", "<leader>xq", ":TroubleToggle quickfix<cr>")
+vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<cr>")
+vim.keymap.set("n", "<F4>", ":Git blame<cr>")
 --
 -- NOTES
 --
