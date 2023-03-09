2362e5395c4abebda75a9c3bf6b5c59e5bfc093d
Dan Strokirk <dan.strokirk@gmail.com>
2023-03-10 00:40:11 +0100

    chore: Clean up docs in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,11 @@
 -- VIMRC SETTINGS
 
--- OPTIONS & SETTINGS
 require("config/settings")
-
--- MAPPINGS, COMMANDS & AUTOCOMMANDS
 require("config/keymaps")
 
--- PLUGINS
--- Load plugins from plugins/init and sibling files
+-- PLUGINS: Loads all modules from ./plugins directory:
+-- nvim/lua/plugins/init.lua
+-- nvim/lua/plugins/lsp.lua
 require("bootstrap")
 require("lazy").setup({ import = "plugins" }, require("lazy_options"))
 
@@ -22,20 +20,3 @@ vim.keymap.set("n", "<leader>xd", ":TroubleToggle document_diagnostics<cr>")
 vim.keymap.set("n", "<leader>xq", ":TroubleToggle quickfix<cr>")
 vim.keymap.set("n", "<leader>xx", ":TroubleToggle<cr>")
 vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<cr>")
-vim.keymap.set("n", "<leader>q", ":copen<cr>")
-
---
--- NOTES
---
--- TODO: Ännu snabbare mapping för viW och viw
--- TODO: Sortera inline ([a, c, b] -> [a, b, c])
--- TODO: Bättre text-obj för [{("'indent
--- TODO: Bättre shortcut for bokmärken
--- TODO: Bättre listning av saker som :reg och :ls
--- TODO: Better quickfix management
--- TODO: map for opening the plugin in github
--- TODO: Autoupdate settings files
--- TODO: require-if-exists('local')
--- TODO: require-if-exists('local-plugins')
-
--- STUDY: Multicursor
