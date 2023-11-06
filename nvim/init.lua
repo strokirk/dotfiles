2ebb1ce018da3f7786bbd57da663176bbb76052f
Dan Strokirk <dan.strokirk@gmail.com>
2023-11-06 11:45:50 +0100

    fix(nvim): Fix Lazy.nvim colors that are too bright
    Unfortunately this doesn't seem easy to hide away in an /after/ file,
    so we need to stick it in the main `init.lua` file.
    
    See: https://github.com/folke/lazy.nvim/discussions/1143

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,3 +11,9 @@ require("lazy").setup({ import = "plugins" }, require("lazy_options"))
 
 -- Colorscheme
 vim.cmd("silent! colorscheme molokai")
+
+-- Override some Lazy.nvim colors to be less bright.
+-- Needs to be done after the `require('lazy')` call,
+-- see https://github.com/folke/lazy.nvim/discussions/1143
+vim.api.nvim_set_hl(0, "LazyDimmed", { link = "Folded" })
+vim.api.nvim_set_hl(0, "LazyProp", { link = "Number" })
