596a62e02d33ed1d6a69ac50756e4fd934f40946
Dan Strokirk <dan.strokirk@gmail.com>
2025-08-25 22:10:11 +0200

    chore: Make nvim plugin files easier to open

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
 -- VIMRC SETTINGS
-
 require("config/settings")
 require("config/keymaps")
 
--- PLUGINS: Loads all modules from ./plugins directory:
--- nvim/lua/plugins/init.lua
--- nvim/lua/plugins/lsp.lua
 require("bootstrap")
-require("lazy").setup({ import = "plugins" }, require("lazy_options"))
+require("lazy").setup({
+  { import = "plugins/init" },
+  { import = "plugins/lsp" },
+  { import = "plugins/colorschemes" },
+}, require("lazy_options"))
 
 -- Colorscheme
 vim.cmd("silent! colorscheme molokai")
